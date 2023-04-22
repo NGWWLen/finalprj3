d72a6de0270a8321f9c18937314af11c7c9506ac
chandradewi26 <69431090+chandradewi26@users.noreply.github.com>
2023-04-22 23:35:12 +1000

    Update ApplicationDbContext

--- PersonalFinalProject/Data/ApplicationDbContext.cs
+++ PersonalFinalProject/Data/ApplicationDbContext.cs
@@ -6,15 +6,17 @@ namespace PersonalFinalProject.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public DbSet<Restaurant> Restaurants { get; set; }
-        public DbSet<Area >Areas { get; set; }
-        public DbSet<RestaurantTable> RestaurantTables { get; set; }
+        public DbSet<Area> Areas { get; set; }
         public DbSet<Reservation> Reservations { get; set; }
-       
-        public DbSet<Sitting> Sittings { get; set; }
         public DbSet<ReservationSource> ReservationSources { get; set; }
         public DbSet<ReservationStatus> ReservationStatuses { get; set; }
-        public DbSet<SittingType>   SittingTypes{ get; set; }
+        public DbSet<ReservationTable> ReservationTables { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<RestaurantTable> RestaurantTables { get; set; }
+        public DbSet<Sitting> Sittings { get; set; }
+        public DbSet<SittingType> SittingTypes { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<SittingType> UserTypes { get; set; }
 
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
