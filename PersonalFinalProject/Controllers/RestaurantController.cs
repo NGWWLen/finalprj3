fd6a6fafe5cce4c490e99b9dfe79124fdc03f7fc
chandradewi26 <69431090+chandradewi26@users.noreply.github.com>
2023-04-27 00:47:18 +1000

    Restaurant

---
+++ PersonalFinalProject/Controllers/RestaurantController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PersonalFinalProject.Data;
+using PersonalFinalProject.Models;
+
+namespace PersonalFinalProject.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        //Private field
+        private ApplicationDbContext _context = new ApplicationDbContext();
+
+        //Constructor
+        public RestaurantController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var restaurant = await _context.Restaurants.ToListAsync();
+            return View(restaurant);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var r = await _context.Restaurants.FirstOrDefaultAsync(r => r.Id == id);
+            if (r == null)
+            {
+                return NotFound();
+            }
+            return View(r);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RestaurantViewModel r)
+        {
+            var newRestaurant = new Restaurant();
+            newRestaurant.Name = r.Name;
+
+            _context.Restaurants.Add(newRestaurant);
+            await _context.SaveChangesAsync();
+            return View();
+        }
+    }
+}
---
+++ PersonalFinalProject/Models/RestaurantViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PersonalFinalProject.Models
+{
+    public class RestaurantViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
